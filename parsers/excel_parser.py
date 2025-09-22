576bcb177c7b28fa4946eedc1fdb9970ae0bfaeb
alpro1000 <alpro1000@gmail.com>
2025-09-22 18:15:51 +0200

    Delete parsers directory

--- parsers/excel_parser.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-def parse_excel(path: str) -> dict[str, pd.DataFrame]:
-    xls = pd.ExcelFile(path)
-    return {sh: xls.parse(sh) for sh in xls.sheet_names}
--- parsers/pdf_parser.py
+++
@@ -1,4 +0,0 @@
-from pdfminer.high_level import extract_text
-
-def parse_pdf(path: str) -> str:
-    return extract_text(path)
