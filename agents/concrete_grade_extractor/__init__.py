e5c7a5bfca570ac098ec78ca09935f1c60973d71
alpro1000 <alpro1000@gmail.com>
2025-09-30 19:11:43 +0200

    Delete agents/concrete_grade_extractor directory

--- agents/concrete_grade_extractor/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Concrete Agent Module - Specialized concrete grade extraction
-"""
-
-from .agent import ConcreteGradeExtractor, ConcreteGrade, get_concrete_grade_extractor
-
-__all__ = ['ConcreteGradeExtractor', 'ConcreteGrade', 'get_concrete_grade_extractor']
