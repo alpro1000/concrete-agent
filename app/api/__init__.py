00c69769e663d01e4fd92c6e49d8b9c813a0ae5d
alpro1000 <alpro1000@gmail.com>
2025-10-09 19:22:25 +0200

    Update __init__.py

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,1 +1,20 @@
+"""
+API Package initialization
+Správné propojení všech routerů
+"""
+from fastapi import APIRouter
 
+# Import všech routerů
+from app.api.routes import router as main_router
+from app.api.routes_workflow_a import router as workflow_a_router
+from app.api.routes_workflow_b import router as workflow_b_router
+
+# Vytvoření hlavního API routeru
+api_router = APIRouter()
+
+# Připojení všech routerů
+api_router.include_router(main_router)
+api_router.include_router(workflow_a_router)
+api_router.include_router(workflow_b_router)
+
+__all__ = ["api_router"]
